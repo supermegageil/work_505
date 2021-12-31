80224fdb28a7221f907286b682da0eb3cd8111bc
supermegageil <kimyeojin133@gmail.com>
2021-12-31 19:25:04 +0900

    01

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import './style.css';
 function App() {
   return (
     <div className="App">
-
+      <Header />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/common/Header.js
@@ -0,0 +1,23 @@
+function Header(){
+    return (
+        <header>
+            <div className="inner">
+                <h1><a href="#">URBAN DETAIL</a></h1>
+
+                <ul id="gnb">
+                    <li><a href="">Department</a></li>
+                    <li><a href="">Community</a></li>
+                    <li><a href="">Gallery</a></li>
+                    <li><a href="">Youtube</a></li>
+                    <li><a href="">Location</a></li>
+                    <li><a href="">Join</a></li>
+                </ul>
+            </div>
+        </header>
+    )
+
+
+
+}
+
+export default Header;
