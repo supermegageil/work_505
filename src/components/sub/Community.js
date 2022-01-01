d58e736359ba0913f9c782a523298aaacbb27e48
supermegageil <kimyeojin133@gmail.com>
2022-01-01 18:26:00 +0900

    06 게시글 컴포넌트 구현

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -1,8 +1,32 @@
+import axios from "axios";
+import {useEffect, useState} from "react";
+
+
 function Community(){
+    let [posts, setPosts] = useState([]);
+    const path= process.env.PUBLIC_URL;
+    const url = `${path}/db/community.json`;
+
+    useEffect(()=>{
+        axios
+            .get(url)
+            .then(json=>{
+                console.log(json.data.data);
+                setPosts(json.data.data);
+            })
+    })
+
     return (
         <main>
             <div className="inner">
                 <h1><a href="#">Community</a></h1>
+                {
+                    posts.map((data, index)=>{
+                        return (
+                            <article key={index}><h1><span>{data.writer}</span><em>{data.date}</em></h1></article>
+                        )
+                    })
+                }
             </div>
         </main>
     )
