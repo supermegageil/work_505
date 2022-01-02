76949f383bf10190729e12b1a038c42840317714
supermegageil <kimyeojin133@gmail.com>
2022-01-02 17:08:04 +0900

    09 팝업 생성, 제거시 스크롤 활성화 유무 제어

--- src/components/sub/Gallery.js
+++ src/components/sub/Gallery.js
@@ -2,12 +2,17 @@
 import axios from "axios";
 import {useEffect, useState} from "react";
 
+const body = document.querySelector("body");
 
 function Gallery(){
     let [items, setItems] = useState([]);
     let [isPop, setIsPop] = useState(false);
+    let [index, setIndex] = useState(0);
+
+
     const api_key="4f0c7aacc8689e7a16060a7f710cf848";
     const url = `https://www.flickr.com/services/rest/?method=flickr.interestingness.getList&api_key=${api_key}&per_page=10&format=json&nojsoncallback=1`;
+
     useEffect(()=>{
         axios
           .get(url)
@@ -30,7 +35,10 @@ function Gallery(){
                             return(
                                 <article key={index}>
                                   <div className="inner">
-                                    <div className="pic" onClick={()=>{setIsPop(true);}}>
+                                    <div className="pic" onClick={()=>{setIsPop(true);
+
+                                      setIndex(index);
+                                      console.log(index);}}>
                                       <img src={imgSrc} />
                                     </div>
               
@@ -47,14 +55,28 @@ function Gallery(){
         </main>
     )
     function Pop(){
+      const imgSrc = `https://live.staticflickr.com/${items[index].server}/${items[index].id}_${items[index].secret}_b.jpg`;
+
+    useEffect(()=>{
+      console.log("pop 생성")
+      body.style.overflow = "hidden";
+
+      return ()=>{
+        console.log("pop 제거");
+        body.style.overflow = "auto";
+        }
+      },[]);  
+
       return (
         <aside className="pop">
+          <img src={imgSrc} />
+          <p>{items[index].title}</p>
           <span onClick={()=>{
             setIsPop(false);
           }}>Close</span>
         </aside>
-      )
-}
+      )            
+  }
 }
 
 export default Gallery;
