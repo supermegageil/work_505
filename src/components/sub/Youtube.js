425410d984ca4449b3f1ed9f397babed1b3f32ac
supermegageil <kimyeojin133@gmail.com>
2022-01-02 17:45:54 +0900

    10 Youtube

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -1,4 +1,27 @@
+// My Play List Id: PLLoUbL4kq1KiWN9_82ViMcIwwkT68RMdN
+// Youtube API KEY: AIzaSyClmDMoUAOVqnfaM5m7fQa7Wrm9yJqBeZ8 
+
+import axios from 'axios';
+import {useEffect, useState} from 'react';
+
 function Youtube(){
+    let [data, setData] = useState([]);
+
+    const api_key = "AIzaSyClmDMoUAOVqnfaM5m7fQa7Wrm9yJqBeZ8";
+    const playListId = "PLLoUbL4kq1KiWN9_82ViMcIwwkT68RMdN";
+    const num = 3;
+    const url =  `https://www.googleapis.com/youtube/v3/playlistItems?part=snippet&key=${api_key}&playlistId=${playListId}&maxResults=${num}`;
+
+    useEffect(()=>{
+        axios
+            .get(url)
+            .then(json=>{
+                console.log(json.data.items);
+                setData(json.data.items);
+            })
+        },[]);
+
+
     return (
         <main>
             <div className="inner">
