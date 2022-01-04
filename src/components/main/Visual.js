47cc88dea3afe6a6d8e4d6b68ed779dc44763d0a
supermegageil <kimyeojin133@gmail.com>
2022-01-04 21:43:43 +0900

    15 visual 수정

--- src/components/main/Visual.js
+++ src/components/main/Visual.js
@@ -1,7 +1,9 @@
-function Visual(){
+function Visual() {
     return (
-        <figure></figure>
+        <figure id="visual">
+        <img className="visualImage" alt="이미지" src="img/arch3.jpeg" />
+    </figure>
+        
     )
 }
-
 export default Visual;
